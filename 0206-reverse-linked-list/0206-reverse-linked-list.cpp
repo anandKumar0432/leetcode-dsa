9be608582d27a5555cd4ee5e009fe3e3e20c762e
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2025-09-30 01:16:59 +0530

    Time: 0 ms (100%), Space: 13.5 MB (39.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -7,7 +7,7 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -20,5 +20,25 @@ public:
             head = next;
         }
         return prev;
+    }
+};
+ */
+class Solution {
+public:
+    ListNode* solve(ListNode* head, ListNode* prev, ListNode* next){
+        if(head == NULL){
+            return prev;
+        }
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+        return solve(head, prev, next);
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        return solve(head, prev, next);
     }
 };
