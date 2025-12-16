8830f6942be1e91d9d3d0e031323f17e6620e6e8
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2025-12-16 10:41:16 +0530

    Time: 0 ms (100%), Space: 9.9 MB (58.94%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,42 @@
+
+//-----------BIT MANIPULATION----------
+
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> res;
+//         vector<int> arr;
+//         int n = nums.size();
+//         for(int i=0; i<(1<<n);i++){
+//             for(int j=0; j<n; j++){
+//                 if((i & (1<<j)) != 0){
+//                     arr.push_back(nums[j]);
+//                 }
+//             }
+//             res.push_back(arr);
+//             arr.clear();
+//         }
+//         return res;
+//     }
+// };
+
+//---------USING RECURSION(BACKTRACKING)------------
 class Solution {
 public:
+    void subsequence(int index, vector<int> &nums, vector<int> &arr, vector<vector<int>> &res){
+        if(index >= nums.size()){
+            res.push_back(arr);
+            return;
+        }
+        arr.push_back(nums[index]);
+        subsequence(index+1, nums, arr, res);
+        erase(arr, nums[index]);
+        subsequence(index+1, nums, arr, res);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> arr;
-        int n = nums.size();
-        for(int i=0; i<(1<<n);i++){
-            for(int j=0; j<n; j++){
-                if((i & (1<<j)) != 0){
-                    arr.push_back(nums[j]);
-                }
-            }
-            res.push_back(arr);
-            arr.clear();
-        }
+        subsequence(0, nums, arr, res);
         return res;
     }
 };
