6d04a37d7aa29ef64cd84947e6782e37189b3ccc
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2025-12-28 19:39:32 +0530

    Time: 3 ms (40.83%), Space: 12.1 MB (96.56%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        for(int i=0; i<nums1.size(); i++){
+            int dig = nums1[i];
+            auto it = find(nums2.begin(), nums2.end(), dig);
+            int idx = distance(nums2.begin(), it);
+            int n = res.size();
+            for(int j=idx+1; j<nums2.size(); j++){
+                if(nums2[j] > nums1[i]){
+                    res.push_back(nums2[j]);
+                    break;
+                }
+            }
+            if(n == res.size())
+                res.push_back(-1);
+        }
+
+        return res;
+    }
+};
