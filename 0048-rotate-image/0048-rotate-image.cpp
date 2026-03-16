49eefa2dd163a6c642f831f1899e2fe5a4c774ff
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-03-16 23:02:37 +0530

    Time: 0 ms (100%), Space: 10.2 MB (36.07%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,23 +1,49 @@
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int row = matrix.size();
+//         int col = matrix[0].size();
+//         int top = 0;
+//         int bottom = row-1;
+
+//         while(top < bottom){
+//             for(int i = 0; i<col; i++){
+//                 swap(matrix[top][i], matrix[bottom][i]);
+//             }
+//             top++;
+//             bottom--;
+//         }
+
+//         for(int i = 0; i<row; i++){
+//             for(int j = i; j<col; j++){
+//                 swap(matrix[i][j], matrix[j][i]);
+//             }
+//         }
+//     }
+// };
+
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        int top = 0;
-        int bottom = row-1;
+        int i = 0;
+        int j = 0;
 
-        while(top < bottom){
-            for(int i = 0; i<col; i++){
-                swap(matrix[top][i], matrix[bottom][i]);
-            }
-            top++;
-            bottom--;
-        }
-
-        for(int i = 0; i<row; i++){
-            for(int j = i; j<col; j++){
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=i; j<matrix.size(); j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
+        }
+
+        i = 0;
+        j = matrix.size() -1;
+
+        while( i < j){
+            for(int k=0; k<matrix.size(); k++){
+                swap(matrix[k][i], matrix[k][j]);
+            }
+            i++;
+            j--;
         }
     }
 };
