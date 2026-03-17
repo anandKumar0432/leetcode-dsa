25c3f0353fd9a3221d1c37e76f46fb63b851c7f0
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-03-18 00:39:24 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -17,3 +17,25 @@ public:
         }
     }
 };
+
+
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int count = 0;
+//         int n = nums.size();
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] == 0){
+//                 count++;
+//                 nums.erase(nums.begin() + i);
+//             }
+//         }
+
+//         while(count){
+//             nums.push_back(0);
+//             count--;
+//         }
+//     }
+// };
