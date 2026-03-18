36e86ba9712458867ab5a752931c04d9571077f7
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-03-18 22:01:51 +0530

    Time: 9 ms (25.66%), Space: 26.4 MB (27.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,68 @@
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//          vector<int> res;
+//          int maj1 = NULL;
+//          int count1 = 0;
+//          int maj2 = NULL;
+//          int count2 = 0;
+//          int n = nums.size();
+
+//          for(int i=0; i<nums.size(); i++){
+//             if(nums[i] == maj1){
+//                 count1++;
+//             }else if(nums[i] == maj2){
+//                 count2++;
+//             }else if(count1 == 0){
+//                 maj1 = nums[i];
+//                 count1++;
+//             }else if(count2 == 0){
+//                 maj2 = nums[i];
+//                 count2++;
+//             }else{
+//                 count1--;
+//                 count2--;
+//             }
+//          }
+
+//          //verify the result
+//          int freq1 = 0;
+//          int freq2 = 0;
+//          for(int &num : nums){
+//             if(num == maj1){
+//                 freq1++;
+//             }else if(num == maj2){
+//                 freq2++;
+//             }
+//          }
+
+//          if(freq1 > floor(n/3)){
+//             res.push_back(maj1);
+//          }
+//          if(freq2 > floor(n/3)){
+//             res.push_back(maj2);
+//          }
+//          return res;
+//     }
+// };
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-         vector<int> res;
-         int maj1 = NULL;
-         int count1 = 0;
-         int maj2 = NULL;
-         int count2 = 0;
-         int n = nums.size();
+        unordered_map<int,int> mp;
+        vector<int> res;
 
-         for(int i=0; i<nums.size(); i++){
-            if(nums[i] == maj1){
-                count1++;
-            }else if(nums[i] == maj2){
-                count2++;
-            }else if(count1 == 0){
-                maj1 = nums[i];
-                count1++;
-            }else if(count2 == 0){
-                maj2 = nums[i];
-                count2++;
-            }else{
-                count1--;
-                count2--;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+
+        int n = nums.size()/3;
+
+        for(auto ele : mp){
+            if(ele.second > n){
+                res.push_back(ele.first);
             }
-         }
+        }
 
-         //verify the result
-         int freq1 = 0;
-         int freq2 = 0;
-         for(int &num : nums){
-            if(num == maj1){
-                freq1++;
-            }else if(num == maj2){
-                freq2++;
-            }
-         }
-
-         if(freq1 > floor(n/3)){
-            res.push_back(maj1);
-         }
-         if(freq2 > floor(n/3)){
-            res.push_back(maj2);
-         }
-         return res;
+        return res;
     }
 };
