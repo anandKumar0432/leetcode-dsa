3b4a1477e812657d0488b24270185cf0c99b8702
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-03-20 01:13:46 +0530

    Time: 7 ms (14.5%), Space: 23.7 MB (11.73%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         string str = "";
+//         string word = "";
+
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i] != ' '){
+//                 word = word+s[i];
+//             }
+//             if((s[i] == ' ' || i == s.size()-1) && !word.empty()){
+//                 str = " " + word + str;
+//                 word = "";
+//             }
+//         }
+//         str.erase(0,1);
+//         return str;
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     string reverseWords(string s) {
@@ -6,14 +32,21 @@ public:
 
         for(int i=0; i<s.size(); i++){
             if(s[i] != ' '){
-                word = word+s[i];
+                word+=s[i];
             }
+
             if((s[i] == ' ' || i == s.size()-1) && !word.empty()){
                 str = " " + word + str;
                 word = "";
             }
         }
-        str.erase(0,1);        
+
+        str.erase(0,1);
+
         return str;
+
     }
 };
+
+
+
