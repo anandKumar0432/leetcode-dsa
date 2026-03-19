84682ed8ab3ed2422ce3914c50a814c477ef5039
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-03-19 23:40:26 +0530

    Time: 0 ms (100%), Space: 9.5 MB (27.52%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,23 +1,70 @@
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         if(s.size() != t.size()){
+//             return false;
+//         }
+//         map<char, char> mpp1;
+//         map<char, char> mpp2;
+//         for(int i=0; i<s.size(); i++){
+//             char a = s[i];
+//             char b = t[i];
+//             if(mpp1.count(a) || mpp2.count(b)){
+//                 if(mpp1[a] != b || mpp2[b] != a){
+//                     return false;
+//                 }
+//             }else{
+//                 mpp1[a] = b;
+//                 mpp2[b] = a;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size() != t.size()){
-            return false;
-        }
-        map<char, char> mpp1;
-        map<char, char> mpp2;
+        unordered_map<char,char> mp1, mp2;
+        if(s.size() != t.size()) return false;
+
         for(int i=0; i<s.size(); i++){
-            char a = s[i];
-            char b = t[i];
-            if(mpp1.count(a) || mpp2.count(b)){
-                if(mpp1[a] != b || mpp2[b] != a){
-                    return false;
-                }
-            }else{
-                mpp1[a] = b;
-                mpp2[b] = a;
+            if(mp1.find(s[i]) == mp1.end()){
+                mp1[s[i]] = t[i];
+            }
+
+            if(mp2.find(t[i]) == mp2.end()){
+                mp2[t[i]] = s[i];
             }
         }
+
+        for(int i=0; i<s.size(); i++){
+            if(mp1[s[i]] != t[i]) return false;
+            if(mp2[t[i]] != s[i]) return false;
+        }
+
         return true;
+
     }
 };
+
+
+
+
+
+
+
+
+
+
+
