fc9f289415ff1ca890e30be05c9cd3eeece135f5
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-03-19 22:09:28 +0530

    Time: 0 ms (100%), Space: 20.6 MB (57.99%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,26 @@
+// class Solution {
+// public:
+//     int num = 0;
+//     int singleNumber(vector<int>& nums) {
+
+//         for(int i=0; i<nums.size(); i++){
+//             num = ( num ^ nums[i]);
+//         }
+//         return num;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int num = 0;
     int singleNumber(vector<int>& nums) {
-
-        for(int i=0; i<nums.size(); i++){
-            num = ( num ^ nums[i]);
+        int num = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            num = num ^ nums[i];
         }
+
         return num;
     }
+    
 };
