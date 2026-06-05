da0c83b89922ea19072166b9ac77e95db8e7f44a
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-06-05 18:11:43 +0530

    Time: 0 ms (100%), Space: 10.8 MB (21.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,53 @@
+// RECURSIVE (DP)
+
+// class Solution {
+// public:
+//     int solve(vector<int>& nums, int idx, vector<int> &dp){
+//         if(idx == 0) return nums[idx];
+//         if(idx < 0) return 0;
+        
+//         if(dp[idx] != -1) return dp[idx];
+
+//         int pick = nums[idx] + solve(nums, idx - 2, dp);
+//         int notPick = solve(nums, idx - 1, dp);
+
+//         return dp[idx] = max(pick, notPick);
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         int size = nums.size();
+//         int idx = nums.size() - 1;
+//         vector<int> dp(size, -1);
+//         return solve(nums, idx, dp);
+//     }
+// };
+
+
+
+
+
+// Tabulation (DP)
+
 class Solution {
 public:
-    int solve(vector<int>& nums, int idx, vector<int> &dp){
-        if(idx == 0) return nums[idx];
-        if(idx < 0) return 0;
-        
-        if(dp[idx] != -1) return dp[idx];
-        
-        int pick = nums[idx] + solve(nums, idx - 2, dp);
-        int notPick = solve(nums, idx - 1, dp);
-
-        return dp[idx] = max(pick, notPick);
-    }
     int rob(vector<int>& nums) {
         int size = nums.size();
-        int idx = nums.size() - 1;
         vector<int> dp(size, -1);
-        return solve(nums, idx, dp);
+        
+        dp[0] = nums[0];
+
+        for(int i = 1; i<size; i++){
+
+            int take = nums[i];
+            if(i-2 >= 0) take += dp[i-2];
+            int notTake = dp[i-1];
+
+            dp[i] = max(take, notTake);
+        }
+
+        return dp[size-1];
     }
 };
+
+
+
