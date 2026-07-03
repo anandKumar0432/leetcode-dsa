f6f71f993594c9fd9a90a7694b273c112faa59b2
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-07-03 23:16:51 +0530

    Time: 29 ms (40.29%), Space: 27.4 MB (68.63%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,42 @@
+// class Solution {
+// public:
+//     int solve(string& text1, string& text2, int idx1, int idx2, vector<vector<int>>& dp){
+
+//         if(idx1 < 0 || idx2 < 0) return 0;
+
+//         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+//         if(text1[idx1] == text2[idx2]){
+//             return dp[idx1][idx2] = 1 + solve(text1, text2, idx1 - 1, idx2 - 1, dp);
+//         }
+//         return dp[idx1][idx2] = max(solve(text1, text2, idx1 - 1, idx2, dp), solve(text1, text2, idx1, idx2 - 1, dp));
+//     }
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n1 = text1.size();
+//         int n2 = text2.size();
+
+//         vector<vector<int>> dp(n1, vector<int>(n2, -1));
+//         return solve(text1, text2, n1-1, n2-1, dp);
+//     }
+// };
+
+
+//////// TABULAR METHOD /////////////////////////////////////////////////
+
 class Solution {
 public:
-    int solve(string& text1, string& text2, int idx1, int idx2, vector<vector<int>>& dp){
-
-        if(idx1 < 0 || idx2 < 0) return 0;
-
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        if(text1[idx1] == text2[idx2]){
-            return dp[idx1][idx2] = 1 + solve(text1, text2, idx1 - 1, idx2 - 1, dp);
-        }
-        return dp[idx1][idx2] = max(solve(text1, text2, idx1 - 1, idx2, dp), solve(text1, text2, idx1, idx2 - 1, dp));
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size();
         int n2 = text2.size();
 
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        return solve(text1, text2, n1-1, n2-1, dp);
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        
+        for(int i=1; i<n1+1; i++){
+            for(int j=1; j<n2+1; j++){
+                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n1][n2];
     }
 };
