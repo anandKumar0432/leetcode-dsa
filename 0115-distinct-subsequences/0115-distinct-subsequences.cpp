84b4bb86d0dacb54a909102b0f190f7ed6efc427
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-07-06 19:40:11 +0530

    Time: 27 ms (69.37%), Space: 44.3 MB (32.47%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -25,27 +25,50 @@
 // };
 
 
+// ---------------MEMOIZATION------------------
 
+// class Solution {
+// public:
+//     int solve(string& s, string& t, int i, int j, vector<vector<int>>& dp){
+
+//         if(j == 0) return 1;
+//         if(i == 0) return 0;
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         if(s[i-1] == t[j-1]){
+//             return dp[i][j] = solve(s, t, i-1, j-1, dp) + solve(s, t, i-1, j, dp);
+//         }
+//         return dp[i][j] = solve(s, t, i-1, j, dp);
+//     }
+//     int numDistinct(string s, string t) {
+//         int n1 = s.size();
+//         int n2 = t.size();
+//         vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+//         return solve(s, t, n1, n2, dp);
+//     }
+// };
+
+
+//---------------TABULATION-------------------
 
 class Solution {
 public:
-    int solve(string& s, string& t, int i, int j, vector<vector<int>>& dp){
-
-        if(j < 0) return 1;
-        if(i < 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i] == t[j]){
-            return dp[i][j] = solve(s, t, i-1, j-1, dp) + solve(s, t, i-1, j, dp);
-        }
-        return dp[i][j] = solve(s, t, i-1, j, dp);
-    }
     int numDistinct(string s, string t) {
         int n1 = s.size();
         int n2 = t.size();
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        return solve(s, t, n1-1, n2-1, dp);
+        vector<vector<double>> dp(n1+1, vector<double>(n2+1, 0));
+        
+        for(int i=0; i<=n1; i++) dp[i][0] = 1;
+
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return (int)dp[n1][n2];
     }
 };
 
