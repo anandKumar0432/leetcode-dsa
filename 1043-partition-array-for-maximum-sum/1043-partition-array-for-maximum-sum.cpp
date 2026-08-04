792b21774be91259b2f922413e17603469e7da46
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-08-04 22:38:55 +0530

    Time: 2 ms (88.88%), Space: 12.5 MB (63.07%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    int solve(vector<int>& arr, int i, int n, int k, vector<int>& dp){
-        if(i == n) return 0;
-        int len = 0;
-        int maxi = INT_MIN;
-        int maxAns = INT_MIN;
+    // int solve(vector<int>& arr, int i, int n, int k, vector<int>& dp){
+    //     if(i == n) return 0;
+    //     int len = 0;
+    //     int maxi = INT_MIN;
+    //     int maxAns = INT_MIN;
 
-        if(dp[i] != -1) return dp[i];
-        for(int j=i; j < min(n, i+k); j++){
-            len++;
-            maxi = max(maxi, arr[j]);
-            int temp = len * maxi + solve(arr, j+1, n, k, dp);
-            maxAns = max(maxAns, temp);
-        }
+    //     if(dp[i] != -1) return dp[i];
+    //     for(int j=i; j < min(n, i+k); j++){
+    //         len++;
+    //         maxi = max(maxi, arr[j]);
+    //         int temp = len * maxi + solve(arr, j+1, n, k, dp);
+    //         maxAns = max(maxAns, temp);
+    //     }
 
-        return dp[i] = maxAns;
-    }
+    //     return dp[i] = maxAns;
+    // }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n, -1);
-        return solve(arr, 0, n, k, dp);
+        vector<int> dp(n+1, -1);
+        // return solve(arr, 0, n, k, dp);
+
+        dp[n] = 0;
+
+        for(int i = n-1; i>=0; i--){
+            int len = 0;
+            int maxi = INT_MIN;
+            int maxAns = INT_MIN;
+
+            for(int j=i; j < min(n, i+k); j++){
+                len++;
+                maxi = max(maxi, arr[j]);
+                int temp = len * maxi + dp[j+1];
+                maxAns = max(maxAns, temp);
+            }
+
+            dp[i] = maxAns;
+        }
+
+        return dp[0];
     }
 };
