e3ca3c002a66bebb076350c0adbf29992aaef039
Anand kumar <126239766+anandKumar0432@users.noreply.github.com>
2026-09-26 15:47:52 +0530

    Time: 0 ms (100%), Space: 68.4 MB (56.77%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,35 +1,71 @@
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+//         int n = ratings.size();
+
+//         vector<int> left(n);
+
+//         left[0] = 1;
+
+//         for(int i=1; i<n; i++){
+//             if(ratings[i] > ratings[i-1]){
+//                 left[i] = left[i-1]+1;
+//             }else{
+//                 left[i] = 1;
+//             }
+//         }
+//         int sum = 0;
+//         int curr = 1;
+//         int next = 1;
+
+//         sum = sum + max(curr, left[n-1]);
+
+//         for(int i=n-2; i>=0; i--){
+//             if(ratings[i] > ratings[i+1]){
+//                 curr = next + 1;
+//                 sum = sum + max(curr, left[i]);
+//                 next = curr;
+//             }else {
+//                 curr = 1;
+//                 next = curr;
+//                 sum = sum + max(curr, left[i]);
+//             }
+//         }
+        
+//         return sum;
+//     }
+// };
+
+
+// ----------------SLOPE METHOD-----------------
+
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-
-        vector<int> left(n);
-
-        left[0] = 1;
-
-        for(int i=1; i<n; i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i] = left[i-1]+1;
-            }else{
-                left[i] = 1;
+        int i = 1;
+        int sum = 1;
+        int peak;
+        int down;
+        while(i<n){
+            while(i<n && ratings[i] == ratings[i-1]) {
+                sum += 1;
+                i++;
             }
-        }
-        int sum = 0;
-        int curr = 1;
-        int next = 1;
+            peak = 1;
 
-        sum = sum + max(curr, left[n-1]);
-
-        for(int i=n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                curr = next + 1;
-                sum = sum + max(curr, left[i]);
-                next = curr;
-            }else {
-                curr = 1;
-                next = curr;
-                sum = sum + max(curr, left[i]);
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak += 1; 
+                sum += peak; 
+                i++;
             }
+            down = 1;
+            while(i<n && ratings[i] < ratings[i-1]) {
+                sum += down;
+                down += 1;
+                i++;
+            }
+            if(down > peak) sum = sum + (down - peak);
         }
         
         return sum;
